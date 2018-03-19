ce8be95d754443d890c4592bb91199ca43e6d106
Ashwini Gupta <Ashwinigupta4455@hotmail.com>
2018-03-19 15:19:55 +0530

    Add project files.

---
+++ Kodos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kodos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
